f911a6b3d3aa738b8c478871aaba02f947b45ea0
DJouve <damien.jouve92@gmail.com>
2019-12-11 14:00:28 +0100

    Adding features country.html

--- scripts/country.js
+++ scripts/country.js
@@ -19,39 +19,68 @@ class Country
         this.country = document.querySelector(".country")
 
 
-        this.changeCountry()
+        this.transition()
         this.initCountry()
     }
 
     initCountry()
     {
-        const imgBg = document.querySelector(".img-background")
-        const imgChara = document.querySelector(".img-character")
-        const imgMonu = document.querySelector(".img-monument")
-        const imgAnimal = document.querySelector(".img-animal")
-        const imgObject = document.querySelector(".img-object")
-        const imgFood = document.querySelector(".img-food")
-        imgBg.setAttribute("src", this.bgSource)
-        imgChara.setAttribute("src", this.charaSource)
-        imgMonu.setAttribute("src", this.monumentSource)
-        imgAnimal.setAttribute("src", this.animalSource)
-        imgObject.setAttribute("src", this.objectSource)
-        imgFood.setAttribute("src", this.foodSource)
+        for (let i = 0; i<7; i++){
+            const imgBg = document.querySelector(".img-background")
+            const imgChara = document.querySelector(".img-character")
+            const imgMonu = document.querySelector(".img-monument")
+            const imgAnimal = document.querySelector(".img-animal")
+            const imgObject = document.querySelector(".img-object")
+            const imgFood = document.querySelector(".img-food")
+            imgBg.setAttribute("src", this.bgSource)
+            imgChara.setAttribute("src", this.charaSource)
+            imgMonu.setAttribute("src", this.monumentSource)
+            imgAnimal.setAttribute("src", this.animalSource)
+            imgObject.setAttribute("src", this.objectSource)
+            imgFood.setAttribute("src", this.foodSource)
+        }
     }
 
-    changeCountry()
+    transition()
     {
         const transition = document.querySelector(".transition")
-        
+        let isOn = true
+
         transition.addEventListener ( 'click', () =>
-            {
-                console.log(this.bgSource)
+        {
+            if(isOn == true) {
+                isOn == false
+                transition.classList.remove('visible')
                 transition.classList.add('invisible')
                 this.country.classList.add('visible')
+                this.country.classList.remove('invisible')
             }
-        )
-        
+        })
+        this.object.addEventListener('click', () =>
+        {
+            isOn == true
+            transition.classList.remove('invisible')
+            transition.classList.add('visible')
+            this.country.classList.remove('visible')
+            this.country.classList.add('invisible')
+            changeCountry()
+        })        
     }
+
+    // changeCountry()
+    // {
+    //     for (let i = 0; i<8; i++)
+    //     {
+
+    //     }
+    // }
+
 }
 
-const country = new Country(document.querySelector(".country"), "../assets/img/JAPON.jpg")
+const Japan = new Country(document.querySelector(".country"), "../assets/img/country/JAPON.jpg")
+// const Russia = new Country(document.querySelector(".country"), )
+// const India = new Country(document.querySelector(".country"), )
+// const Mexico = new Country(document.querySelector(".country"), )
+// const Kenya = new Country(document.querySelector(".country"), )
+// const Egypt = new Country(document.querySelector(".country"), )
+// const Scotland = new Country(document.querySelector(".country"), )
