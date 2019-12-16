f72ca2c3b8c3ebc9f57eccac9063e1aa256e9e5c
DJouve <damien.jouve92@gmail.com>
2019-12-16 14:43:53 +0100

    final japan flower

--- scripts/japan.js
+++ scripts/japan.js
@@ -1,34 +1,34 @@
-const flowersContainer = document.querySelectorAll(".flower-js")
+const flowersContainer = document.querySelector(".flower-js")
 // const flowerContainerLeft = document.querySelector(".container-left")
 // const flowerContainerTop = document.querySelector(".container-top")
 // const flowerContainerRight = document.querySelector(".container-right")
 // const flowerContainerBottom = document.querySelector(".container-bottom")
 
 const container = document.querySelector(".container-flower")
-const flowerClass = [".left-transition", ".top-transition", ".right-transition", ".bottom-transition"]
-for (let i=0; i<flowersContainer.length; i++)
+for (let j=0; j<150; j++)
 {
-    for (let j=0; j<150; j++)
+    let flower = document.createElement("img")
+    let flowerdiv = document.createElement("div")
+    flower.classList.add("exist")
+    flowerdiv.appendChild(flower)
+    flowersContainer.appendChild(flowerdiv)
+    flower.setAttribute("src", "../assets/img/sakura.svg")
+    flowerdiv.classList.add("flower")
+    flowerdiv.style.left = Math.floor(Math.random()*100) + "%"
+    flowerdiv.style.top = Math.floor(Math.random()*100) + "%"
+}
+const flowers = document.querySelectorAll(".flower")
+for (let i = 0; i<flowers.length; i++)
+{
+    flowers[i].addEventListener("mouseover", () => 
     {
-        let flower = document.createElement("img")
-        let flowerdiv = document.createElement("div")
-        flower.classList.add("exist")
-        flowerdiv.appendChild(flower)
-        flowersContainer[i].appendChild(flowerdiv)
-        flower.setAttribute("src", "../assets/img/sakura.svg")
-        flowerdiv.classList.add("flower")
-        flowerdiv.classList.add(flowerClass[i])
-        flowerdiv.style.left = Math.floor(Math.random()*100) + "%"
-        flowerdiv.style.top = Math.floor(Math.random()*100) + "%"
-    }
-    flowersContainer[i].addEventListener("mouseover", () => 
-    {
-        flowersContainer[i].classList.add("animation-flower")
+        flowers[i].classList.add("animation-flower")
         setTimeout(() => {
-            flowersContainer[i].classList.add("display-none")
+            flowers[i].classList.add("display-none")
         }, 1500)
     })
 }
+
 
 const imgBgIntro = document.querySelector(".background-intro-image")
 const bgIntro = document.querySelector(".intro")
