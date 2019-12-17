1dc052ae525542ad3a5ab23da3461be613267936
DJouve <damien.jouve92@gmail.com>
2019-12-17 14:12:36 +0100

    portal and beginning responsive egypt kenya

---
+++ scripts/egypt.js
@@ -0,0 +1,13 @@
+
+const imgBgIntro = document.querySelector(".background-intro-image")
+const bgIntro = document.querySelector(".intro")
+
+imgBgIntro.addEventListener("click", () =>
+{
+    imgBgIntro.classList.add("animation-intro")
+    bgIntro.classList.add("subanimation-intro")
+    setTimeout(() =>
+    {
+        bgIntro.classList.add("display-none")
+    }, 1900)
+})
