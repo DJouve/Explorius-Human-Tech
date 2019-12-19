2a2acf8af4b086f678cf85e51f3685fa15d03a47
zoevincent <zoe.vincent@hetic.net>
2019-12-19 10:46:35 +0100

    :ok_hand: scotland done

--- scripts/scotland.js
+++ scripts/scotland.js
@@ -1,5 +1,12 @@
 const nessieContainer = document.querySelector('.js-nessie-container')
+const popUp = document.querySelector('.js-pop-up')
+const button = document.querySelector('.js-button')
 
 nessieContainer.addEventListener('click', () => {
-    console.log('bravo')
+    popUp.classList.remove('hidden')
+    button.addEventListener('click', () => {
+        popUp.classList.add('hidden')
+        nessieContainer.classList.remove('nessie-anim')
+        nessieContainer.classList.add('nessie-not-anim')
+    })
 })
